7910161985b3ce2397142211a04a105175bb62a2
Arun Rajesh <75040337+arun-qikfox@users.noreply.github.com>
2025-10-17 16:43:02 +0530

    Fix: npm run build error fix

---
+++ shared/types/framer-motion.d.ts
@@ -0,0 +1,17 @@
+declare module 'framer-motion' {
+  export const motion: any;
+  export const AnimatePresence: any;
+  export const LayoutGroup: any;
+  export const Reorder: any;
+  export const useAnimation: any;
+  export const useAnimationControls: any;
+  export const useInView: any;
+  export const useMotionValue: any;
+  export const useScroll: any;
+  export const useSpring: any;
+  export const useTransform: any;
+  export const useVelocity: any;
+  export const m: any;
+  const FramerMotion: any;
+  export default FramerMotion;
+}
