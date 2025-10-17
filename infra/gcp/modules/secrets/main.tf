e09617ffe2c9afacc1ed6e99312827913e0df4fb
Arun Rajesh <75040337+arun-qikfox@users.noreply.github.com>
2025-10-17 14:18:36 +0530

    Fix: terramind config for secrets

--- infra/gcp/modules/secrets/main.tf
+++ infra/gcp/modules/secrets/main.tf
@@ -5,13 +5,14 @@ resource "google_secret_manager_secret" "secrets" {
   secret_id = each.key
 
   replication {
-    automatic {}
+    auto {}
   }
 }
 
 resource "google_secret_manager_secret_version" "secrets" {
   for_each = var.secrets
 
-  secret      = google_secret_manager_secret.secrets[each.key].name
+  # Use the full resource name of the secret:
+  secret      = google_secret_manager_secret.secrets[each.key].id
   secret_data = each.value
 }
